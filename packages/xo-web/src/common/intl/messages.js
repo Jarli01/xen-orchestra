b665f2f4e22159ec92d4eed173d555370d32ddbb
Mohamedox <ghemid_m_dz@yahoo.fr>
2019-06-13 14:00:32 +0200

    fix spelling

--- packages/xo-web/src/common/intl/messages.js
+++ packages/xo-web/src/common/intl/messages.js
@@ -573,7 +573,7 @@ const messages = {
   newSrLocalPathPlaceHolder: '/path/to/directory',
   newSrUseNfs4: 'Use NFSv4',
   newSrNfsOptions: 'Comma delimited NFS options',
-  noZfsSharedAvailable: 'No ZFS shared available',
+  noSharedZfsAvailable: 'No shared ZFS available',
   reattachNewSrTooltip: 'Reattach SR',
   srLocation: 'Storage location',
 
